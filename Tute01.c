5294d305c8205761c358a76c4d53726ffd96a3da
IT21327308 <99328944+IT21327308@users.noreply.github.com>
2022-02-09 11:57:03 +0000

    what did you change

--- Tute01.c
+++ Tute01.c
@@ -5,17 +5,6 @@
 #include <stdio.h>
 
 int main() {
-   int mark1,mark2;
-   float avg;
-   
-   printf("Enter mark 1: ");
-   scanf("%d",&mark1);
-   printf("\nEnter mark 2: ");
-   scanf("%d",&mark2);
-   
-   avg=(mark1+mark2)/2.0;
-   
-   printf("\naverage of two marks is: %.2f",avg);
   
   return 0;
 }
